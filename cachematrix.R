842a2ef342134b9e3be4783b0da630155b7042bf
cvanderbyl <cvanderbyl@users.noreply.github.com>
2015-08-23 12:37:56 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function will create a special "matrix" object that can cache its inverse.
+## 
+rm(list=ls())
+data=matrix(c(5,1,4,2,5,6,10,12,11),3,3)
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix=function(x = matrix()) {
+  m=NULL#                                 Set m to an empty set in makeCacheMatrix environment
+  set=function(y) {                       #Create function to set data
+    x<<-y                                 #assign x to y in a higher environment
+    m<<-NULL                              #assign m to null in the higher environment
+  }
+  get=function(){x}                       #Create function get to return x
+  setinverse=function(solve){m<<-solve}   #Set inverse function to solve by assigning the solve function to m in a higher environment
+  getinverse=function(){m}                #retrive m in the function environment
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
+## Utilise cachematrix to return inverse
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachesolve <- function(x, ...) {        #Set cache solve function with ability to pass arguments to correspoonding function
+  m <- x$getinverse()                   #retrive getinverse function from make cache matrix function
+  if(!is.null(m)) {                     #
+    message("getting cached inverse")
+    return(m)
+  }
+  data <- x$get()                       #Retrive data from the get function in cache solve function
+  m <- solve(data, ...)                 #invert the data retrieved from the cache
+  x$setinverse(m)                       #Set the inverse of the cache matrix
+  m                                     #Return the inverted matrix
 }
+
+cachesolve(makeCacheMatrix(data))     #Demonstration
+
